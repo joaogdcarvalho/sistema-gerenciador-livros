f2ffdb455655aa82fa1425c3a199ebb1ea86b186
joaogdcarvalho <27590118+joaogdcarvalho@users.noreply.github.com>
2023-09-25 22:31:51 -0300

    refactor: Refatora configuração de inicialização do servidor em server.js

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import app from './src/app.js';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Servidor escutando em http://localhost:${port}`);
-});
+app
+  .listen(port, () => {
+    console.log(`API já está funcionando e aceitando requisições!`);
+    console.log(`Servidor escutando em http://localhost:${port}`);
+  })
+  .on("error", (err) => {
+    console.error(`Erro ao iniciar o servidor: ${err.message}`);
+  });
