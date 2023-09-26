266313a27eafa4563f5f079ed71f78d831e50622
joaogdcarvalho <27590118+joaogdcarvalho@users.noreply.github.com>
2023-09-25 21:38:37 -0300

    refactor: melhora organização e tratamento de erros no código do servidor - app.js

--- src/app.js
+++ src/app.js
@@ -4,41 +4,40 @@ const app = express();
 
 app.use(express.json());
 
+// Rotas
 app.get("/", (req, res) => {
   res.status(200).send("Home");
 });
 
-const livros = [
-  {id: 1, "titulo": "Garota Exemplar"},
-  {id: 2, "titulo": "Barba ensopada de sangue"}
-]
-
-
-app.get('/livros', (req, res) => {
+app.get("/livros", (req, res) => {
   res.status(200).json(livros);
 });
 
 app.get("/livros/:id", (req, res) => {
-  let index = buscaLivro(req.params.id);
-  res.json(livros[index]);
+  const livro = buscaLivro(req.params.id);
+  if (livro) {
+    res.json(livros);
+  } else {
+    res.status(404).send("Livro não encontrado");
+  }
 });
 
-app.post('/livros', (req, res) => {
-  livros.push(req.body);
-  res.status(201).send('Livro cadastrado com sucesso');
+app.post("/livros", (req, res) => {
+  const novoLivro = req.body;
+  livros.push(novoLivro);
+  res.status(201).send("Livro cadastrado com sucesso");
 });
 
-app.put('/livros/:id', (req, res) => {
-  let index = buscaLivro(req.params.id);
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivroIndex(req.params.id);
 
-  livros[index].titulo = req.body.titulo;
-
-  res.json(livros);
+  if(index !== -1){
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+  } else {
+    res.status(404).send("Livro não encontrado");
+  }  
 });
-
-function buscaLivro(id) {
-  return livros.findIndex(livro => livro.id == id);
-}
 
 app.get("/autores", (req, res) => {
   res.status(200).send("Autores");
@@ -52,5 +51,18 @@ app.get("/sobre", (req, res) => {
   res.status(200).send("Sobre");
 });
 
+// Funções de Apoio
+const livros = [
+  {id: 1, "titulo": "Garota Exemplar"},
+  {id: 2, "titulo": "Barba ensopada de sangue"}
+];
+
+function buscaLivro(id) {
+  return livros.find(livro => livro.id == id);
+}
+
+function buscaLivroIndex(id) {
+  return livros.findIndex(livro => livro.id == id);
+}
 
 export default app;
