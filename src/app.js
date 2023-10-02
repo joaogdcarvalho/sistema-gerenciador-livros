893f2a3b1ee1f10809bf11e4b0758a0e63728193
joaogdcarvalho <27590118+joaogdcarvalho@users.noreply.github.com>
2023-10-02 04:07:39 -0300

    refactor: remove métodos desnecessários do script app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import db from "./config/dbConnect.js";
-import livros from "./models/Livro.js";
 import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
@@ -14,35 +13,5 @@ const app = express();
 app.use(express.json());
 
 routes(app);
-
-// Rotas
-app.get("/autores", (req, res) => {
-  res.status(200).send("Autores");
-});
-
-app.get("/editora", (req, res) => {
-  res.status(200).send("Editora");
-});
-
-app.get("/sobre", (req, res) => {
-  res.status(200).send("Sobre");
-});
-
-function buscaLivro(id) {
-  const index = livros.findIndex((livro) => livro.id == id);
-  if (index !== -1) {
-    return { livro: livros[index], index: index };
-  } else {
-    return { livro: null, index: -1 };
-  }
-}
-
-function validaLivro(livro) {
-  return livro && livro.titulo;
-}
-
-function livroNaoEncontrado(res) {
-  res.status(404).send("Livro não encontrado");
-}
 
 export default app;
