35e9459c8c727b9010e6220e0fbd95b61a7f7093
joaogdcarvalho <27590118+joaogdcarvalho@users.noreply.github.com>
2023-10-03 01:50:34 -0300

    feat: adiciona associação de dados entre schemas - objeto autor do livro

--- src/controller/livrosControllers.js
+++ src/controller/livrosControllers.js
@@ -2,7 +2,9 @@ import livros from "../models/Livro.js";
 
 class LivroController {
   static listarLivros = (req, res) => {
-    livros.find((err, livros) => {
+    livros.find()
+    .populate('autor')
+    .exec((err, livros) => {
       res
         .status(200)
         .json(livros);
@@ -12,15 +14,17 @@ class LivroController {
   static listarLivroPorId = (req, res) => {
     const id = req.params.id;
 
-    livros.findById(id, (err, livros) => {
-      if(err){
-        res
-          .status(400)
-          .send({message: `${err.message} - Id '${id}' não localizado.`})
-      } else {
-        res
-          .status(200)
-          .json(livros)
+    livros.findById(id)
+      .populate('autor', 'nome')
+      .exec((err, livros) => {
+        if(err){
+          res
+            .status(400)
+            .send({message: `${err.message} - Id '${id}' não localizado.`})
+        } else {
+          res
+            .status(200)
+            .json(livros)
       };
     });
   };
